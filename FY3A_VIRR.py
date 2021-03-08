2c7358bf0691fff81de15044fe1bb14c85a10af7
LittlematchLM <529289524@qq.com>
2021-03-08 16:41:56 +0800

    20210308 修改FY3A 读取VIRR sst程序

--- FY3A_VIRR.py
+++ FY3A_VIRR.py
@@ -53,8 +53,10 @@ crs2 = CRS(proj="aeqd")
 
 transformer = HaiYangData.set_transformer(crs,crs2)
 transformer_back = HaiYangData.set_transformer(crs2,crs)
-test_list = [file_list[3][14:17]]
-for i,files in enumerate(file_list[con_point:]):
+test_list = [file_list[3][11:17]]
+
+
+for i,files in enumerate(test_list[con_point:]):
     grid_array = np.zeros((fy_virr.nlat, fy_virr.nlon))
     grid_num_array = np.zeros((fy_virr.nlat, fy_virr.nlon))
     day = files[0].split('//')[-1].split(r'_')[7]
@@ -70,40 +72,50 @@ for i,files in enumerate(file_list[con_point:]):
             right_top_lon = fh.getncattr('Right-Top Longitude')
             left_bottom_lon = fh.getncattr('Left-Bottom Longitude')
             right_bottom_lon = fh.getncattr('Right-Bottom Longitude')
-
             sst = fh.variables['VIRR_SST'][:]
 
-            lats, lons = np.mgrid[ left_top_lat:left_bottom_lat:1000j,left_top_lon:right_top_lon:1000j]
+        if left_top_lon < 0 :
+            left_top_lon += 360
+        if right_top_lon < 0:
+            right_top_lon += 360
+        if left_bottom_lon < 0:
+            left_bottom_lon += 360
+        if right_bottom_lon < 0:
+            right_bottom_lon += 360
+            
+        lats, lons = np.mgrid[ left_top_lat:left_bottom_lat:1000j,left_top_lon:right_top_lon:1000j]
 
-            # projlats, projlons = transformer.transform(lats, lons)
+        # projlats, projlons = transformer.transform(lats, lons)
 
-            value_array = np.empty(shape=(lons.shape[0], lons.shape[1],5))
+        value_array = np.empty(shape=(lons.shape[0], lons.shape[1],5))
 
-            value_array[:,:,0] = lats
-            value_array[:,:,1] = lons
-            value_array[:,:,2],value_array[:,:,3] = transformer.transform(value_array[:,:,0], value_array[:,:,1])
-            value_array[:,:,4] = sst
+        value_array[:,:,0] = lats
+        value_array[:,:,1] = lons
+        value_array[:,:,2],value_array[:,:,3] = transformer.transform(value_array[:,:,0], value_array[:,:,1])
+        value_array[:,:,4] = sst
 
 
-            x = (value_array[:,:,2] / fy_virr.resolution).astype(np.int)
-            y = (value_array[:,:,3] / fy_virr.resolution).astype(np.int)
+        x = (value_array[:,:,2] / fy_virr.resolution).astype(np.int)
+        y = (value_array[:,:,3] / fy_virr.resolution).astype(np.int)
 
-            grid_array[y,x] += value_array[:,:,4]
-            grid_num_array[y,x] += 1
-    # 获得XYmgrid
-            grid_array = grid_array / grid_num_array
-            x_map, y_map = fy_virr.get_map_grid(transformer_back)
+        grid_array[y,x] += value_array[:,:,4]
+        grid_num_array[y,x] += 1
+# 获得XYmgrid
+        grid_array = grid_array / grid_num_array
+
+        print(file)
 
             # x_map += 150
 
 # 将非法点置为np.nan
-    grid_array[grid_array==0] = np.nan
+    grid_array[grid_array<-100] = np.nan
 
+    x_map, y_map = fy_virr.get_map_grid(transformer_back)
     plt.figure(figsize=(16, 9))
-    hy_m = Basemap(projection='npaeqd', boundinglat=60, lon_0=0, resolution='c')
-    hy_m.pcolormesh(x_map, y_map, data=grid_array, cmap=plt.cm.jet,vmin=0,vmax=100,latlon = True)
+    hy_m = Basemap(projection='npaeqd', boundinglat=50, lon_0=0, resolution='c')
+    hy_m.pcolormesh(x_map, y_map, data=grid_array, cmap=plt.cm.jet,vmin=0,vmax=300,latlon = True)
     hy_m.colorbar(location='right')
-    hy_m.fillcontinents()
+    # hy_m.fillcontinents()
     hy_m.drawmapboundary()
     hy_m.drawparallels(np.arange(-90., 120., 10.), labels=[1, 0, 0, 0])
     hy_m.drawmeridians(np.arange(-180., 180., 60.), labels=[0, 0, 0, 1])
@@ -111,18 +123,18 @@ for i,files in enumerate(file_list[con_point:]):
     plt.show()
 
 
-            plt.savefig(save_path +r'pic//'+ file_name + '.jpg')
-            plt.close()
+    plt.savefig(save_path +r'pic//'+ file_name + '.jpg')
+    plt.close()
 
 
-        with Dataset(save_path + file_name + '.nc', 'w', format='NETCDF4') as file:
-            file.createDimension('x', grid_array.shape[0])
-            file.createDimension('y', grid_array.shape[0])
-            icecon_north = file.createVariable('icecon_north', 'i4', dimensions=('x', 'y'))
-            lat = file.createVariable('lat', 'f4', dimensions=('x', 'y'))
-            lon = file.createVariable('lon', 'f4', dimensions=('x', 'y'))
-            icecon_north[:] = grid_array
-            lon[:] = x_map
-            lat[:] = y_map
-        print(str(i+con_point) + '//' + str(len(files)))
-        print(satellite +'_'+ sensor +'_'+ day)
+    with Dataset(save_path + file_name + '.nc', 'w', format='NETCDF4') as file:
+        file.createDimension('x', grid_array.shape[0])
+        file.createDimension('y', grid_array.shape[0])
+        icecon_north = file.createVariable('icecon_north', 'i4', dimensions=('x', 'y'))
+        lat = file.createVariable('lat', 'f4', dimensions=('x', 'y'))
+        lon = file.createVariable('lon', 'f4', dimensions=('x', 'y'))
+        icecon_north[:] = grid_array
+        lon[:] = x_map
+        lat[:] = y_map
+    print(str(i+con_point) + '//' + str(len(files)))
+    print(satellite +'_'+ sensor +'_'+ day)
